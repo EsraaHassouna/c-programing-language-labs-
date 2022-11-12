e3a3ec184bc6fcba242fda9409887cbfc9386933
EsraaHassouna <esraa.hassouna9.11@gmail.com>
2022-11-13 00:44:26 +0200

    pointer to array

--- lab6/lab6.1/main.c
+++ lab6/lab6.1/main.c
@@ -8,21 +8,18 @@ void displayArray(int *ptr);
 int main()
 {
     int i=0;
-    int arr[size]={0,1,2,3};
-    int *ptr=arr[size];
+    int arr[size];
+    int *ptr=&arr;
 
     for (i=0;i<size;i++){
-        displayArray(arr);
+      printf("enter the array elements ");
+      scanf("%d",&arr[i]);
     }
+
+    for (i=0;i<size;i++){
+      printf("the array elements %d \n",*ptr);
+      ptr++;
+      }
+
     return 0;
-}
-/*void scanArray(int *ptr){
-    int y=0;
-    ptr=&y;
-    printf("enter the numbers \n");
-    scanf("%d",y);
-}
-*/
-void displayArray(int *ptr){
-printf("the arr is %d - ",*ptr);
 }
