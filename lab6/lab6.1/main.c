b9ce76a63fb50a3b0be962682a298e1a96315ae9
EsraaHassouna <esraa.hassouna9.11@gmail.com>
2022-11-12 03:44:29 +0200

    update rec

---
+++ lab6/lab6.1/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define size 4
+
+void scanArray(int *ptr);
+void displayArray(int *ptr);
+
+int main()
+{
+    int i=0;
+    int arr[size]={0,1,2,3};
+    int *ptr=arr[size];
+
+    for (i=0;i<size;i++){
+        displayArray(arr);
+    }
+    return 0;
+}
+/*void scanArray(int *ptr){
+    int y=0;
+    ptr=&y;
+    printf("enter the numbers \n");
+    scanf("%d",y);
+}
+*/
+void displayArray(int *ptr){
+printf("the arr is %d - ",*ptr);
+}
